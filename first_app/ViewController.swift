f3e44fa7a1c751352b4792b30828df5434dfc97d
Jacek Baczkowski <jacekbaczkowski@v3492-0acf2317.wifi.cf.ac.uk>
2016-05-26 23:28:18 +0100

    Initial Commit

---
+++ first_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  first_app
+//
+//  Created by Jacek Baczkowski on 26/05/2016.
+//  Copyright © 2016 Jacek Baczkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
